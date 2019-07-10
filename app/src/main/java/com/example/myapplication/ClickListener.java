6c2e1486b922f962c9e883e6558b9bda9d073396
tawsif93 <tawsif93@gmail.com>
2019-07-10 22:27:33 +0600

    update last class

---
+++ app/src/main/java/com/example/myapplication/ClickListener.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.view.View;
+
+/**
+ * Created by tawsif93 on 2019-07-08.
+ * 19:46
+ */
+public class ClickListener implements View.OnClickListener {
+
+	@Override
+	public void onClick(View view) {
+
+	}
+}
